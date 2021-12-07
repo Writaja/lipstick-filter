f1b7694ab98e34808d5523c29be769edec361c00
Writaja <74670011+Writaja@users.noreply.github.com>
2021-12-07 22:14:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,29 @@ function preload(){
 
 }
 function setup(){
-canvas=createCanvas(300,300);
-canvas.center();
+    function setup(){
+        canvas=createCanvas(300,300);
+        canvas.center();
+        video =createCapture(VIDEO);
+        video.size(300,300);
+        video.hide();
+        poseNet=ml5.poseNet(video,modelloaded);
+        poseNet.on("pose",gotposes);
+        }
 }
 function draw(){
-    
+    image(video,0,0,300,300);
     }
     function take_snapshot(){
         
+    }
+    function modelloaded(){
+        console.log("poseNet is initialized");
+    }
+    function gotposes(result){
+        if(result.length>0){
+            console.log(result);
+            console.log("nose x = "+result[0].pose.nose.x);
+            console.log("nose y = "+result[0].pose.nose.y);
+        }
     }
