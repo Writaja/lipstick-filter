05d891b4c150595261615246213f25d8e211669c
Writaja <74670011+Writaja@users.noreply.github.com>
2021-12-09 21:47:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
 
+lipx=0;
+lipsy=0;
 function preload(){
-
+img=loadImage("https://postimg.cc/21VQssFW");
 }
+
 function setup(){
-    function setup(){
-        canvas=createCanvas(300,300);
-        canvas.center();
-        video =createCapture(VIDEO);
-        video.size(300,300);
-        video.hide();
-        poseNet=ml5.poseNet(video,modelloaded);
-        poseNet.on("pose",gotposes);
-        }
+canvas=createCanvas(300,300);
+canvas.center();
+video =createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet=ml5.poseNet(video,modelloaded);
+poseNet.on("pose",gotposes);
 }
 function draw(){
-    image(video,0,0,300,300);
+image(video,0,0,300,300);
+//fill(255,0,0);
+//stroke(255,0,0);
+//circle(nosex,nosey,20);
+image(img,nosex,nosey,30,30);
+}
+function take_snapshot(){
+    save("filterimg.png");
+}
+function modelloaded(){
+    console.log("poseNet is initialized");
+}
+function gotposes(result){
+    if(result.length>0){
+        console.log(result);
+        console.log("lip x = "+result[0].pose.lip.x);
+        console.log("lip y = "+result[0].pose.lip.y);
+        lipx=result[0].pose.nose.x-10;
+        lipy=result[0].pose.nose.y-10;
     }
-    function take_snapshot(){
-        
-    }
-    function modelloaded(){
-        console.log("poseNet is initialized");
-    }
-    function gotposes(result){
-        if(result.length>0){
-            console.log(result);
-            console.log("nose x = "+result[0].pose.nose.x);
-            console.log("nose y = "+result[0].pose.nose.y);
-        }
-    }
+}
